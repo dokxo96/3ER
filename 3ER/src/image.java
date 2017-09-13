0e98a1a4133c74f49db07251018eea83100f8648
dokxo96 <dokxo96@gmail.com>
2017-09-12 18:24:42 -0600

    se agregaron mejoras a la interfaz

---
+++ 3ER/src/image.java
@@ -0,0 +1,39 @@
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dokxo
+ */
+	
+public class image extends javax.swing.JPanel {
+ 
+public image() {
+this.setSize(450, 350); //se selecciona el tamaño del panel
+}
+ 
+//Se crea un método cuyo parámetro debe ser un objeto Graphics
+ 
+public void paint(Graphics grafico) {
+Dimension height = getSize();
+ 
+//Se selecciona la imagen que tenemos en el paquete de la //ruta del programa
+ 
+ImageIcon Img = new ImageIcon(getClass().getResource("images.jpg")); 
+ 
+//se dibuja la imagen que tenemos en el paquete Images //dentro de un panel
+ 
+grafico.drawImage(Img.getImage(), 0, 0, height.width, height.height, null);
+ 
+setOpaque(false);
+super.paintComponent(grafico);
+}
+}
